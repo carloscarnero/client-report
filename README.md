9530b6a18e53dda65e039ae388586ee60ad1068e
Carlos Alberto Carnero Delgado <carloscarnero@gmail.com>
2023-12-14 10:57:49 -0500

    Project generated from template

---
+++ README.md
@@ -0,0 +1,2 @@
+This is a simple [Next.js](https://nextjs.org/) project that should report
+the client IP address that made the HTTP request.
---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import type { FunctionComponent } from "react";
+
+const Page: FunctionComponent = () => {
+  return (
+    <main>
+      <p>
+        Hello, world!
+      </p>
+    </main>
+  );
+};
+
+export default Page;
